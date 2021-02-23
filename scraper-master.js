35ce1a8ba52571af2939b4d4c3dabd3d10859e9b
ludotosk <toscanoludovico@gmail.com>
2021-02-23 16:01:53 +0100

    puliti dati

--- scraper-master.js
+++ scraper-master.js
@@ -5,10 +5,28 @@ const inizio = Date.now();
 
 var array = []
 
-var pagePrimo
-var pageSecondo
+async function main(page, tipo_laurea) {
+    await page.setRequestInterception(true);
 
-async function main(page) {
+    //if the page makes a  request to a resource type of image or stylesheet then abort that request
+    page.on('request', request => {
+        if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() == 'font' || request.resourceType() == 'media')
+            request.abort();
+        else
+            request.continue();
+    });
+
+    await page.goto('https://www.universitaly.it/index.php/public/cercaOffPL');
+
+    await page.evaluate(tipo_laurea => {
+        document.querySelector('#tipo_laurea').value = tipo_laurea;
+    }, tipo_laurea);
+
+    await page.click('.btn-form')
+
+    await page.waitForSelector('#ui-accordion-colonna-dx-panel-1 > table > tbody > tr > td:nth-child(4)')
+
+    console.log('caricato ' + tipo_laurea)    
 
     const risultati = await page.evaluate(() => {
         const linee = document.querySelectorAll('tr');
@@ -19,19 +37,25 @@ async function main(page) {
             const universita = linee[i].querySelectorAll('h3')[0]
             if (universita) {
                 uni = universita.textContent
+                if (uni == 'LUM "Jean Monnet"') {
+                    uni = 'Libera università mediterranea Giuseppe Degennaro'
+                }
+                if (uni == 'Università degli Studi di BOLOGNA') {
+                    uni = 'Alma mater studiorum Università di Bologna'
+                }
             } else {
                 const corsoObj = linee[i].querySelectorAll('b')[0]
                 if (corsoObj) {
-                    let corso = corsoObj.innerText
+                    let corso = corsoObj.innerText.replaceAll('\"','')
 
                     const testo = linee[i].textContent
                     var citta = /(?<=, )[A-Z']+ [A-Z']+ [A-Z']+(?=  )|(?<=, )[A-Z']+(?=  )|(?<=, )[A-Z']+ [A-Z']+(?=  )|(?<=, )[A-Z']+ [A-Z']+ [A-Z']+ [A-Z']+(?=  )|(?<=, )[A-Z']+ - [A-Z']+(?=  )/gm.exec(testo)
 
                     citta = citta[0]
 
-                    const durata = linee[i].getElementsByClassName('icona')[2].getElementsByTagName('img')[0].getAttribute('title')
+                    const durata = linee[i].getElementsByClassName('icona')[2].getElementsByTagName('img')[0].getAttribute('title').slice(8)
 
-                    const lingua = linee[i].getElementsByClassName('icona')[3].getElementsByTagName('img')[0].getAttribute('title')
+                    const lingua = linee[i].getElementsByClassName('icona')[3].getElementsByTagName('img')[0].getAttribute('title').slice(8)
 
                     const link = linee[i].getElementsByTagName('a')[1].href
 
@@ -47,78 +71,25 @@ async function main(page) {
 
     page.close();
 
+    console.log('lunghezza risultati: ' + risultati.length)
+
     array = array.concat(risultati)
-    //return risultati;
-}
-
-async function apriM1() {
-    await pagePrimo.setRequestInterception(true);
-
-    //if the page makes a  request to a resource type of image or stylesheet then abort that request
-    pagePrimo.on('request', request => {
-        if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() == 'font' || request.resourceType() == 'media')
-            request.abort();
-        else
-            request.continue();
-    });
-
-    await pagePrimo.goto('https://www.universitaly.it/index.php/public/cercaOffPL');
-
-    await pagePrimo.evaluate(() => {
-        document.querySelector('#tipo_laurea').value = 'M1';
-    });
-
-    await pagePrimo.click('.btn-form');
-
-    await pagePrimo.waitForSelector('#ui-accordion-colonna-sx-header-0')
-
-    console.log('caricato primo livello')
-}
-
-async function apriM2() {
-    await pageSecondo.setRequestInterception(true);
-
-    //if the page makes a  request to a resource type of image or stylesheet then abort that request
-    pageSecondo.on('request', request => {
-        if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() == 'font' || request.resourceType() == 'media')
-            request.abort();
-        else
-            request.continue();
-    });
-
-    await pageSecondo.goto('https://www.universitaly.it/index.php/public/cercaOffPL');
-
-    await pageSecondo.evaluate(() => {
-        document.querySelector('#tipo_laurea').value = 'M2';
-    });
-
-    await pageSecondo.click('.btn-form');
-
-    await pageSecondo.waitForSelector('#ui-accordion-colonna-sx-header-0')
-
-    console.log('caricato secondo livello')
-
-    //return Promise.resolve;
 }
 
 async function laucnhScrape() {
-    const browser = await puppeteer.launch({ headless: true, });
-    pagePrimo = await browser.newPage();
-    pageSecondo = await browser.newPage();
-
-    console.log('pagine create')
-
-    await Promise.all([apriM1(), apriM2()])
+    const browser = await puppeteer.launch({ headless: true });
+    const pagePrimo = await browser.newPage();
+    const pageSecondo = await browser.newPage();
 
     console.log('tabelle caricate')
 
-    await Promise.all([main(pagePrimo), main(pageSecondo)])
+    await Promise.all([main(pagePrimo, 'M1'), main(pageSecondo, 'M2')])
 
-    console.log('dati presi')
+    console.log('numero di corsi: ' + array.length)
 
     browser.close();
 
-    fs.writeFile('./src/master.json', JSON.stringify(array), function (err) {
+    fs.writeFile('./master.json', JSON.stringify(array), function (err) {
         if (err) return console.log(err);
         console.log('master > master.json');
         const fine = (Date.now() - inizio) / 1000;
